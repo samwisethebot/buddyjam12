eb7838266581ed91027cc656e33166e9941f3b70
Samwisethebot <samwisethebot@protonmail.com>
2024-03-08 16:35:22 -0500

    update build + 3d camera movement
    Updated Build Settings
    Added TextMeshPro assets
    Added MainMenu scene prototype
    Added GameplayScene with a 3d player camera movement prototype

---
+++ buddyjam12/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    //defining camera movement
+
+    //mouse sensitivity
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation; 
+
+    //camera rotation
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+    }
+
+
+}
