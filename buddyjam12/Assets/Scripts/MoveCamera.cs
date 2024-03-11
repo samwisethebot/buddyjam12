ed385cd820e1f849d3519e1b2b4cb73c4183e84c
ruspe <cypekarek@gmail.com>
2024-03-11 19:49:28 -0400

    Basic environment started
    Made some adjustments to the player as well for better scale

--- buddyjam12/Assets/Scripts/MoveCamera.cs
+++ buddyjam12/Assets/Scripts/MoveCamera.cs
@@ -7,6 +7,6 @@ public class MoveCamera : MonoBehaviour
     public Transform cameraPosition;
     private void Update()
     {
-        transform.position = cameraPosition.position;
+      //  transform.position = cameraPosition.position;
     }
 }
